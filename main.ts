fe03dc09549705705edbff9d02e0b35d2ddf60a2
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-11-03 18:35:34 +0000

    define blank functions

--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+    SendDsitanceAndSpeed()
+    basic.pause(500)
+    Connected = true
+    CarCtrl()
+    DoSpeed()
+})
+function SendDsitanceAndSpeed () {
+	
+}
+function DoSpeed () {
+	
+}
+function CarCtrl () {
+	
+}
+let Connected = false
 bluetooth.setTransmitPower(7)
 bluetooth.startUartService()
 basic.showIcon(IconNames.StickFigure)
-let Connected = false
+Connected = false
 let Speed = 100
